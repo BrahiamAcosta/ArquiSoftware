6ce19ff750c1667c2e174cbe7a85da028f76e390
BrahiamAcosta <brahiam.acosta@udea.edu.co>
2024-10-01 23:47:25 -0500

    Added controller and code for unit test

--- Laboratorio2/CICDApplication/src/test/java/com/udea/cicdapplication/CicdApplicationTests.java
+++ Laboratorio2/CICDApplication/src/test/java/com/udea/cicdapplication/CicdApplicationTests.java
@@ -1,13 +1,65 @@
 package com.udea.cicdapplication;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.udea.cicdapplication.controller.DataController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class CicdApplicationTests {
 
+    @Autowired
+    DataController dataController;
+
     @Test
-    void contextLoads() {
+    public void health(){
+        assertEquals("HEALTH CHECK OK",dataController.healtCheck());
+    }
+
+    @Test
+    public void version(){
+        assertEquals("THE ACTUAL VERSION IS 1.0.0", dataController.version());
+    }
+
+    @Test
+    public void nationsLength(){
+        Integer nationsLength = dataController.getRandomNations().size();
+        assertEquals(10,nationsLength);
+    }
+
+    @Test
+    public void currenciesLength(){
+        Integer currenciesLength = dataController.getRandomCurrencies().size();
+        assertEquals(20,currenciesLength);
+    }
+
+    @Test
+    public void aviationLength(){
+        Integer aviationLength = dataController.getRandomAviation().size();
+        assertEquals(20,aviationLength);
+    }
+
+    @Test
+    public void testRandomCurrenciesCodeFormat(){
+        JsonNode response = dataController.getRandomCurrencies();
+        for(int i=0;i<response.size();i++){
+            JsonNode currency = response.get(i);
+            String code = currency.get("code").asText();
+            assertTrue(code.matches("[A-Z]{3}"));
+        }
+    }
+
+    @Test
+    public void testRandomNationsPerformance(){
+        long startTime = System.currentTimeMillis();
+        dataController.getRandomNations();
+        long endTime = System.currentTimeMillis();
+        long executionTime = endTime-startTime;
+        System.out.println(executionTime);
+        assertTrue(executionTime<2000);
     }
 
 }
